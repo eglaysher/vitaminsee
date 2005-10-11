3a4233310bd37d44bfc36a906a5ba8ca94ca356c
Elliot Glaysher <glaysher@umich.edu>
2005-10-11 23:54:34 +0000

    Copying header...

---
+++ ToolbarDelegate.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////
+// File:          $Name$
+// Module:        Category responsible for managing the toolbar
+// Part of:       VitaminSEE
+//
+// Revision:      $Revision$
+// Last edited:   $Date$
+// Author:        $Author$
+// Copyright:     (c) 2005 Elliot Glaysher
+// Created:       1/30/05
+//
+/////////////////////////////////////////////////////////////////////////
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+////////////////////////////////////////////////////////////////////////
+
+
+#import "VitaminSEEController.h"
+
+@interface VitaminSEEController (ToolbarDelegate)
+
+-(void)setupToolbar;
+
+- (NSToolbarItem *) toolbar: (NSToolbar *)toolbar
+	  itemForItemIdentifier: (NSString *) itemIdent 
+  willBeInsertedIntoToolbar:(BOOL) willBeInserted;
+
+- (NSArray *) toolbarDefaultItemIdentifiers: (NSToolbar *) toolbar;
+- (NSArray *) toolbarAllowedItemIdentifiers: (NSToolbar *) toolbar;
+@end
